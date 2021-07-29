2db1af13e8300d3f25bc24042854fa7cc47fd601
Hope Ciuffa <hopeciuffa@gmail.com>
2021-07-28 19:34:02 -0500

    declared variables

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -4,19 +4,28 @@
 
 // Part 1: Declare (5) Variables With Type
 
-
+let spacecraftName: string = "Determination";
+let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
 
 // Part 2: Print Days to Mars
-
-
+let milesToMars: number =  kilometersToMars * milesPerKilometer;
+let hoursToMars: number =  milesToMars / speedMph;
+let daysToMars: number =  hoursToMars / 24;
 
 // Code an output statement here (use a template literal):
-
+console.log(`${spacecraftName} will arrive in ${daysToMars} days.`);
 
 
 // Part 3: Create a Function ("getDaysToLocation")
-
-
+function getDaysToLocation(kilometersAway:number): number {
+return 
+}
+function myFunction(x: number): number {
+    return x*2;
+ }
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
